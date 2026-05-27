28b004e77d73cb257a10284e7076dd6fcdc06a0d
Joel <joelriccaalvarez@gmail.com>
2026-05-27 19:50:19 +0200

    actualizacion de caché al agregar modificar o eliminar productos en la base

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,8 @@
 import { HomePage } from "@/components/home/home-page";
 import { getProducts } from "@/application/get-products";
 
+export const revalidate = 3600;
+
 export default async function Home() {
   const products = await getProducts();
   return <HomePage products={products} />;
